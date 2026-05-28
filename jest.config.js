159c89de213dd9f1703aed87ff542112ba3e195f
Giorgi Khatiashvili <57389796+giorgikh93@users.noreply.github.com>
2026-05-28 15:45:28 +0000

    fix tests (#13)

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,9 @@ export default {
   testEnvironment: 'jsdom',
   transform: {
     '^.+\\.[jt]sx?$': 'babel-jest'
+  },
+  moduleNameMapper: {
+    '^@tetherto/pear-apps-utils-generate-unique-id$':
+      '<rootDir>/../pear-apps-utils-generate-unique-id/src/index.js'
   }
 }
